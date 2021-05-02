6ae4f21427e6e626e62403e352aab001ff1bbe09
Kaoru Tsuda <kaoru.tsuda@bitkey.jp>
2021-05-02 16:31:09 +0900

    capture screen shot if test failed

--- __test__/test.py
+++ __test__/test.py
@@ -15,27 +15,42 @@ class test_selenium(unittest.TestCase):
         self.driver = drivers.getDriver()
 
     def test_show_page(self):
+        execute_scenario(self.driver, self.show_page)
+
+    def show_page(self):
         driver = self.driver
         basic.show_page(driver)
         header = driver.find_element_by_xpath('//*[@id="root"]')
         self.assertIsNotNone(header)
 
     def test_show_logout_button(self):
+        execute_scenario(self.driver, self.show_logout_button)
+
+    def show_logout_button(self):
         driver = self.driver
 
         basic.show_page(driver)
         is_exist_logout_button = element_is_missing(driver,
                                                     '//*[@id="menu-appbar"]/div[3]/ul/li')
 
-        self.assertFalse(is_exist_logout_button)
+        self.assertTrue(is_exist_logout_button, "Logout Button is not exist")
 
         basic.click_account_icon(driver)
         logoutButton = driver.find_element_by_xpath(
             '//*[@id="menu-appbar"]/div[3]/ul/li')
-        self.assertTrue(logoutButton.is_enabled())
+        self.assertFalse(logoutButton.is_enabled())
 
     def tearDown(self):
         self.driver.close()
+
+
+def execute_scenario(driver, fn):
+    try:
+        fn()
+    except Exception as e:
+        print("Exception in test: ", e)
+        driver.get_screenshot_as_file('screens/ss-%s.png' % fn.__name__)
+        raise e
 
 
 def element_is_missing(driver, xpath):
@@ -43,7 +58,6 @@ def element_is_missing(driver, xpath):
         driver.find_element_by_xpath(xpath)
         return True
     except selenium.common.exceptions.NoSuchElementException as err:
-        print("could not find element in xpath ", err)
         return False
     else:
         print(arg, 'has', len(f.readlines()), 'lines')
